1d6af6ef724ec8de8cb70fdff9c567e9057abe2a
Carl Davidson <carl.davidson@pioneer.com>
2019-08-20 00:14:27 -0500

    copy pasting academics functions from tectonics.js

---
+++ inc/academics/academics.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#define CPP
+
+#include "cross_platform_macros.c"
+#include "academics/units.c"
+#include "academics/math/constants.c"
+#include "academics/math/geometry.c"
+#include "academics/physics/constants.c"
+#include "academics/physics/emission.c"
+#include "academics/physics/scattering.c"
+#include "academics/physics/reflectance.c"
+#include "academics/graphics/raymarching.c"
+#include "academics/graphics/psychophysics.c"
+#include "academics/graphics/electronics.c"
