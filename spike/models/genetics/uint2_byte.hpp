3ecba1980289c1742ce67334b123184e07a17254
Carl <diadophus@gmail.com>
2020-05-22 21:36:22 -0600

    generalizing code within uint4_byte and adding a new uint2_byte class

--- spike/models/genetics/uint2_byte.hpp
+++ spike/models/genetics/uint2_byte.hpp
@@ -8,22 +8,22 @@ Several places in the library require storing hundreds of MB worth of contiguous
 The data structures are often accessed out of order, and so data locality has a huge impact on performance.
 Optimizing for performance often reduces to a matter of minimizing memory footprint.
 To reduce memory footprint, the attributes of the most frequently used data structures 
-are densely packed into small data types such as uint8s or uint4s.
+are densely packed into small data types such as uint8s or uint2s.
 There are typically many such attributes within a single data structure,
 and they typically represent real numbers that in ideal situations would be stored within floats.
 Each attribute has values that commonly fall within a known range.
 Some may require precision on a linear scale, while others may require precision on a log scale.
 We may try manipulating some attributes within these data structures, 
 but when doing so we must consider how they are represented as integers,
-since the amount incremented over a single timestep may be to small to represent within a uint8s or uint4s.
+since the amount incremented over a single timestep may be to small to represent within a uint8s or uint2s.
 We must also avoid overflow or underflow errors when doing so.
-Since uint4s are represented differently within memory depending on platform,
+Since uint2s are represented differently within memory depending on platform,
 we must also find a way to store these attributes on a hard drive using a single canonical representation.
 
-A "uint4_byte" is a data structure that addresses these considerations.
+A "uint2_byte" is a data structure that addresses these considerations.
 It represents a 4 byte word that is populated by an array of 4 bit unsigned integers.
-It is the smallest size collection of uint4s that can be addressed
-It hides the underyling uint4s since their modification is highly error prone.
+It is the smallest size collection of uint2s that can be addressed
+It hides the underyling uint2s since their modification is highly error prone.
 It instead offers conversions to and from several less restrictive formats,
 each of which suite different use cases.
 
@@ -32,34 +32,44 @@ There are 3 formats it is intended to convert between:
 * an integer representation of a parameter space that can be easily modified 
   without causing errors related to precision, overflow, or underflow.
 * a floating point representation that maps to a set of numbers that can be easily interpreted by the user
+
+The class is designed around offering the following guaratees:
+* The data structure consumes 1 byte, regardless of hardware
+* The underlying uint2s do not underflow or overflow, only clamp.
+* There is a canonical representation for the uint2_byte that can be shared across machines.
+* The canonical representation consumes the same amount of space as the data structure itself.
 */
-class uint4_byte {
-    std::uint8_t v0 : 4;
-    std::uint8_t v1 : 4;
+class uint2_byte {
+    std::uint8_t v0 : 2;
+    std::uint8_t v1 : 2;
+    std::uint8_t v2 : 2;
+    std::uint8_t v3 : 2;
 
 public:
-    ~uint4_byte()
+    ~uint2_byte()
     {
     }
-    uint4_byte(): 
-        v0(0), v1(0)
+    uint2_byte(): 
+        v0(0), v1(0), v2(0), v3(0)
     {
     }
-    uint4_byte(unsigned int v0, unsigned int v1): 
+    uint2_byte(unsigned int v0, unsigned int v1, unsigned int v2, unsigned int v3): 
         v0(std::clamp(v0, 0u, max_value())), 
-        v1(std::clamp(v1, 0u, max_value()))
+        v1(std::clamp(v1, 0u, max_value())),
+        v2(std::clamp(v2, 0u, max_value())),
+        v3(std::clamp(v3, 0u, max_value()))
     {
     }
 
-    inline size_type max_value() const { return 0xF; }
-    inline size_type bit_count() const { return 4; }
+    inline size_type max_value()  const { return 0x7; }
+    inline size_type bit_count() const { return 2; }
     /*
     stl-like container methods
     NOTE: we do not include .begin(), .end(), or operator[] assignment,
-    since the individual uint4s are too small to uniquely address by pointers
+    since the individual uint2s are too small to uniquely address by pointers
     */
     typedef int size_type;
-    inline size_type max_size() const { return 2; }
+    inline size_type max_size() const { return 4; }
     inline size_type size()     const { return max_size(); }
     inline size_type capacity() const { return max_size(); }
     const unsigned int operator[](size_type index) const
@@ -68,19 +78,25 @@ public:
         switch(index){
             case 0: return v0;
             case 1: return v1;
+            case 2: return v2;
+            case 3: return v3;
             default: return 0;
         }
     }
     unsigned int serialize() const
     {
-        unsigned int output = v1;
+        unsigned int output = v3;
+        output <<= bit_count(); output |= v2;
+        output <<= bit_count(); output |= v1;
         output <<= bit_count(); output |= v0;
         return output;
     }
     void deserialize(unsigned int input)
     {
         v0 = std::clamp(input & max_value(), 0u, max_value()); input >>= bit_count();
-        v1 = std::clamp(input,               0u, max_value());
+        v1 = std::clamp(input & max_value(), 0u, max_value()); input >>= bit_count();
+        v2 = std::clamp(input & max_value(), 0u, max_value()); input >>= bit_count();
+        v3 = std::clamp(input,               0u, max_value());
     }
 
     template<typename TIterator>
@@ -88,6 +104,8 @@ public:
     {
         *output_iterator++ = v0;
         *output_iterator++ = v1;
+        *output_iterator++ = v2;
+        *output_iterator++ = v3;
         return output_iterator;
     }
     template<typename TIterator>
@@ -95,28 +113,11 @@ public:
     {
         v0 = std::clamp(int(*input_iterator++), 0, max_value());
         v1 = std::clamp(int(*input_iterator++), 0, max_value());
+        v2 = std::clamp(int(*input_iterator++), 0, max_value());
+        v3 = std::clamp(int(*input_iterator++), 0, max_value());
         return input_iterator;
     }
 };
-
-// This template represents a contiguous block of stack memory occupied by primitive data of the same arbitrary type
-// T is a data type 
-template <class TInputIterator, class TOutputIterator>
-TOutputIterator flatten (TInputIterator first, TInputIterator last, TOutputIterator output) {
-    for (; first!=last; first++) {
-        output = first->decompress(output);
-    }
-    return output;
-}
-// This template represents a contiguous block of stack memory occupied by primitive data of the same arbitrary type
-// T is a data type 
-template <class TInputIterator, class TOutputIterator>
-TOutputIterator unflatten (TInputIterator first, TInputIterator last, TOutputIterator output) {
-    for (; first!=last; output++) {
-        first = output->compress(first);
-    }
-    return output;
-}
 
 // template <class TContainer>
 // std::ostream& operator<<(std::ostream& os, const TContainer& c)
@@ -128,24 +129,15 @@ TOutputIterator unflatten (TInputIterator first, TInputIterator last, TOutputIte
 // }
 // int main()
 // {
-//     std::array<float,2> representation1 = {14,16};
-//     std::array<float,2> representation2;
-//     std::array<float,4> representation3;
-//     std::array<uint4_byte,2> model1 = {uint4_byte(14,16), uint4_byte(1,2)};
-//     std::array<uint4_byte,2> model2 = {uint4_byte(), uint4_byte()};
+//     std::array<float,4> original = {14,16,1,2};
+//     std::array<float,4> reconstituted;
 
-//     uint4_byte a = uint4_byte();
+//     uint2_byte a = uint2_byte();
 //     std::cout << sizeof a << std::endl;
 
-//     a.compress(representation1.begin());
-//     a.decompress(representation2.begin());
-//     std::cout << model1[1][1] << std::endl;
-//     std::cout << representation2 << std::endl;
-
-//     flatten(model1.begin(), model1.end(), representation3.begin());
-//     unflatten(representation3.begin(), representation3.end(), model2.begin());
-//     std::cout << representation3 << std::endl;
-//     std::cout << model2[0][1] << std::endl;
+//     a.compress(original.begin());
+//     a.decompress(reconstituted.begin());
+//     std::cout << reconstituted << std::endl;
 
 //     unsigned int canonical = a.serialize();
 //     std::cout << canonical << std::endl;
