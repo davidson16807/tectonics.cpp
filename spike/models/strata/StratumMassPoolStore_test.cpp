ecaf15bc2018b9a335ec48b2e4120d3c69b6b6f4
Carl Davidson <davids14@illinoisalumni.org>
2020-08-01 20:08:35 -0600

    reusing StratumMassPool_test_utils.hpp

--- spike/models/strata/StratumMassPoolStore_test.cpp
+++ spike/models/strata/StratumMassPoolStore_test.cpp
@@ -3,42 +3,22 @@
 #include <catch/catch.hpp>
 
 #include "StratumMassPoolStore.hpp"
+#include "StratumMassPool_test_utils.hpp"
 
 using namespace strata;
 
 TEST_CASE( "StratumMassPoolStore compress/decompress invertibility", "[strata]" ) {
   	std::mt19937 generator(2);
 
-	StratumMassPool original;
-	original.mass = generator();
-	for (int j = 0; j < grain_type_count; ++j)
-	{
-		original.grain_type_fractional_volume[j] = generator();
-	}
-	float total_fractional_volume(0);
-	for (int j = 0; j < grain_type_count; ++j)
-	{
-		total_fractional_volume += original.grain_type_fractional_volume[j];
-	}
-	for (int j = 0; j < grain_type_count; ++j)
-	{
-		original.grain_type_fractional_volume[j] /= total_fractional_volume;
-	}
-
+	StratumMassPool original = get_random_stratum_mass_pool(generator);
+	
 	StratumMassPoolStore stratum_mass_pool_store;
 	stratum_mass_pool_store.compress(original);
 
 	StratumMassPool reconstructed;
 	stratum_mass_pool_store.decompress(reconstructed);
 
-    SECTION("compressing a StratumMassPool object then decompressing it must reproduce the original object's mass to within 4 decimal places"){
-    	CHECK(reconstructed.mass == Approx(original.mass).epsilon(1e-4));
+    SECTION("compressing a StratumMassPool object then decompressing it must reproduce the original object to within acceptable tolerances"){
+    	APPROXIMATES(reconstructed, original);
 	}
-
-    SECTION("compressing a StratumMassPool object then decompressing it must reproduce the original object's fractional grain sizes to within 1%"){
-		for (int j = 0; j < grain_type_count; ++j)
-		{
-    		CHECK(reconstructed.grain_type_fractional_volume[j] == Approx(original.grain_type_fractional_volume[j]).margin(0.01));
-    	}
-    }
 }
