2cee8ee17f4c3389a3e9ecf7e3891657bf8a640c
Carl Davidson <davids14@illinoisalumni.org>
2021-01-24 22:37:06 -0700

    bug fixes to si unit formatting

--- inc/si.hpp
+++ inc/si.hpp
@@ -210,7 +210,7 @@ namespace si{
 				T1 prefix_id = floor(log10(raw)/(3.0*M1));
 		    	std::string prefixed_value = std::to_string(raw / pow(1000.0, M1*std::min(8.0, prefix_id)));
 		    	std::string prefix(prefixes[int(prefix_id)+8]);
-    			return prefixed_value + (M1<0? "1/":"") + prefix + "m" + (M1!=1? std::to_string(abs(M1)) : "");
+    			return prefixed_value + " " + (M1<0? "1/":"") + prefix + "m" + (M1!=1? std::to_string(abs(M1)) : "");
     		}
 	    	// customize formatting for mass, which must be converted to grams
     		if( KG1 != 0 && (M1|S1|K1|MOL1|A1|CD1) == 0)
@@ -218,7 +218,7 @@ namespace si{
 				T1 prefix_id = floor(log10(raw)/(3.0*KG1));
 		    	std::string prefixed_value = std::to_string(raw / pow(1000.0, KG1*std::min(8.0, prefix_id)));
 		    	std::string prefix(prefixes[int(prefix_id)+KG1+8]);
-    			return prefixed_value + (KG1<0? "1/":"") + prefix + "g" + (KG1!=1? std::to_string(abs(KG1)) : "");
+    			return prefixed_value + " " + (KG1<0? "1/":"") + prefix + "g" + (KG1!=1? std::to_string(abs(KG1)) : "");
     		}
 
 	    	// customize formatting for common derived units for pretty printing
@@ -240,13 +240,13 @@ namespace si{
 					T1 prefix_id = floor(log10(raw)/3.0);
 			    	std::string prefixed_value =  std::to_string(raw / pow(1000.0, std::min(8.0, prefix_id)));
 			    	std::string prefix(prefixes[int(prefix_id)+8]);
-	    			std::string result = prefixed_value + prefix;
+	    			std::string result = prefixed_value + " " + prefix;
 	    			result += named_mks[i].second;
 			    	result +=   K1>0? "K"  + (  K1>1? std::to_string(abs(K1  )) : "") : ""; 
 			    	result += MOL1>0? "mol"+ (MOL1>1? std::to_string(abs(MOL1)) : "") : ""; 
 			    	result +=   A1>0? "A"  + (  A1>1? std::to_string(abs(  A1)) : "") : ""; 
 			    	result +=  CD1>0? "Cd" + ( CD1>1? std::to_string(abs( CD1)) : "") : ""; 
-			    	result += K1<0 || MOL1<0? "/" : "";
+			    	result += K1<0 || MOL1<0 || A1<0 || CD1<0? "/" : "";
 			    	result +=   K1<0? "K"  + (  K1<1? std::to_string(abs(K1  )) : "") : ""; 
 			    	result += MOL1<0? "mol"+ (MOL1<1? std::to_string(abs(MOL1)) : "") : ""; 
 			    	result +=   A1<0? "A"  + (  A1<1? std::to_string(abs(  A1)) : "") : ""; 
@@ -256,8 +256,8 @@ namespace si{
 	    	}
 
 	    	// fall back on SI base units 
-			std::string result = std::to_string(raw);
-	    	result += M1<0 && KG1<0 && S1<0 && K1<0 && MOL1<0? "1" : "";
+			std::string result = std::to_string(raw) + " ";
+	    	result += M1<0 && KG1<0 && S1<0 && K1<0 && MOL1<0 && A1<0 && CD1<0? "1" : "";
 	    	result +=   M1>0? "m"  + (  M1>1? std::to_string(abs(M1  )) : "") : ""; 
 	    	result +=  KG1>0? "kg" + ( KG1>1? std::to_string(abs(KG1 )) : "") : ""; 
 	    	result +=   S1>0? "s"  + (  S1>1? std::to_string(abs(S1  )) : "") : ""; 
@@ -265,7 +265,7 @@ namespace si{
 	    	result += MOL1>0? "mol"+ (MOL1>1? std::to_string(abs(MOL1)) : "") : ""; 
 	    	result +=   A1>0? "A"  + (  A1>1? std::to_string(abs(  A1)) : "") : ""; 
 	    	result +=  CD1>0? "Cd" + ( CD1>1? std::to_string(abs( CD1)) : "") : ""; 
-	    	result += M1<0 || KG1<0 || S1<0 || K1<0 || MOL1<0? "/" : "";
+	    	result += M1<0 || KG1<0 || S1<0 || K1<0 || MOL1<0 || A1<0 || CD1<0? "/" : "";
 	    	result +=   M1<0? "m"  + (  M1<1? std::to_string(abs(M1  )) : "") : ""; 
 	    	result +=  KG1<0? "kg" + ( KG1<1? std::to_string(abs(KG1 )) : "") : ""; 
 	    	result +=   S1<0? "s"  + (  S1<1? std::to_string(abs(S1  )) : "") : ""; 
@@ -714,6 +714,8 @@ namespace si{
 
 	constexpr pressure meter_mercury (1000.0 * 133.322387415);
 	SI_DEPRECATED_PRE_1960_PREFIXED_UNITS(pressure, meter_mercury)
+
+	constexpr pressure atmosphere ( 101325.0 );
 
 	constexpr acceleration  gal (acceleration(0.1));
 	SI_DEPRECATED_PRE_1960_PREFIXED_UNITS(acceleration, gal)
