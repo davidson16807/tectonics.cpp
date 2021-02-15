6e8cd4a5cfce66231ef34c6e00f3c0e858568926
Carl Davidson <davids14@illinoisalumni.org>
2021-02-15 01:06:51 -0700

    helper function for molecular absorption cross sections

--- inc/math/lerp.hpp
+++ inc/math/lerp.hpp
@@ -10,19 +10,19 @@ namespace math
     /*
     "mix" duplicates glm::mix so that we don't have to require glm just to use it.
     */
-    template <typename T, typename T2>
-    constexpr T mix(const T x, const T y, const T2 a) {
-        return x*(T2(1)-a) + y*a;
+    template <typename Tout, typename Tin>
+    constexpr Tout mix(const Tout edge0, const Tout edge1, const Tin a) {
+        return edge0*(Tin(1)-a) + edge1*a;
     }
 
     /*
     "linearstep" provides a strictly linear alternative to glm::smoothstep()
     */
-    template <typename T>
-    constexpr auto linearstep(const T edge0, const T edge1, const T x) 
+    template <typename Tin>
+    constexpr auto linearstep(const Tin edge0, const Tin edge1, const Tin x) 
     {
-        auto fraction = (x - edge0) / (edge1 - edge0);
-        return fraction > T(1)? 1 : fraction < T(0)? 0 : fraction;
+        double fraction = double((x - edge0) / (edge1 - edge0));
+        return fraction > 1.0? 1 : fraction < 0.0? 0 : fraction;
     }
 
     /*
@@ -30,13 +30,13 @@ namespace math
     given a list of control points mapping 1d space to 1d scalars, 
     and a point in 1d space, returns a 1d scalar that maps to the point
     */
-    template <typename T>
-    constexpr typename T::value_type lerp(
-        const T control_points_x, 
-        const T control_points_y, 
-        const typename T::value_type x
+    template <typename Tx, typename Ty>
+    constexpr typename Ty::value_type lerp(
+        const Tx control_points_x, 
+        const Ty control_points_y, 
+        const typename Tx::value_type x
     ) {
-        typename T::value_type result = control_points_y[0];
+        typename Ty::value_type result = control_points_y[0];
         for (std::size_t i = 1; i < control_points_x.size(); i++) 
         {
             result = mix(result, control_points_y[i], linearstep(control_points_x[i-1], control_points_x[i], x));
