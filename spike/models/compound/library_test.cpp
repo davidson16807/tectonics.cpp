302a7b3c9861eb419536604498764bb3218dd514
Carl Davidson <davids14@illinoisalumni.org>
2021-12-06 23:20:54 -0700

    expanding unit tests to other pressures at room temp and standard temp

--- spike/models/compound/library_test.cpp
+++ spike/models/compound/library_test.cpp
@@ -10,46 +10,13 @@
 TEST_CASE( "CompletedCompound order of magnitude", "[field]" ) {
     SECTION("Properties of compounds should not fall outside orders of magnitude for known values")
     {
-        COMPLETED_COMPOUND_VALID(compound::library::water);
-        COMPLETED_COMPOUND_VALID(compound::library::hydrogen);
-        COMPLETED_COMPOUND_VALID(compound::library::helium);
-        COMPLETED_COMPOUND_VALID(compound::library::argon);
-        COMPLETED_COMPOUND_VALID(compound::library::nitrogen);
-        COMPLETED_COMPOUND_VALID(compound::library::oxygen);
-        COMPLETED_COMPOUND_VALID(compound::library::ammonia);
-        COMPLETED_COMPOUND_VALID(compound::library::ozone);
-        COMPLETED_COMPOUND_VALID(compound::library::sulfur_dioxide);
-        COMPLETED_COMPOUND_VALID(compound::library::nitrous_oxide);
-        COMPLETED_COMPOUND_VALID(compound::library::nitric_oxide);
-        COMPLETED_COMPOUND_VALID(compound::library::carbon_dioxide);
-        COMPLETED_COMPOUND_VALID(compound::library::carbon_monoxide);
-        COMPLETED_COMPOUND_VALID(compound::library::hydrogen_cyanide);
-        COMPLETED_COMPOUND_VALID(compound::library::methane);
-        COMPLETED_COMPOUND_VALID(compound::library::perflouromethane);
-        COMPLETED_COMPOUND_VALID(compound::library::formaldehyde);
-        COMPLETED_COMPOUND_VALID(compound::library::formic_acid);
-        COMPLETED_COMPOUND_VALID(compound::library::benzene);
-        COMPLETED_COMPOUND_VALID(compound::library::pyrimidine);
-        COMPLETED_COMPOUND_VALID(compound::library::ethane);
-        COMPLETED_COMPOUND_VALID(compound::library::ethanol);
-        COMPLETED_COMPOUND_VALID(compound::library::quartz);
-        COMPLETED_COMPOUND_VALID(compound::library::corundum);
-        COMPLETED_COMPOUND_VALID(compound::library::carbon);
-        COMPLETED_COMPOUND_VALID(compound::library::orthoclase);
-        COMPLETED_COMPOUND_VALID(compound::library::andesine);
-        COMPLETED_COMPOUND_VALID(compound::library::augite);
-        COMPLETED_COMPOUND_VALID(compound::library::forsterite);
-        COMPLETED_COMPOUND_VALID(compound::library::hematite);
-        COMPLETED_COMPOUND_VALID(compound::library::goethite);
-        COMPLETED_COMPOUND_VALID(compound::library::magnetite);
-        COMPLETED_COMPOUND_VALID(compound::library::pyrite);
-        COMPLETED_COMPOUND_VALID(compound::library::chalcocite);
-        COMPLETED_COMPOUND_VALID(compound::library::chalcopyrite);
-        COMPLETED_COMPOUND_VALID(compound::library::copper);
-        COMPLETED_COMPOUND_VALID(compound::library::silver);
-        COMPLETED_COMPOUND_VALID(compound::library::gold);
-        COMPLETED_COMPOUND_VALID(compound::library::halite);
-        COMPLETED_COMPOUND_VALID(compound::library::calcite);
-        COMPLETED_COMPOUND_VALID(compound::library::apatite);
+        COMPLETED_COMPOUNDS_STP_VALID()
+        for (float p = 1.0; p <= 100000000.0; p*=10.0)
+        {
+            COMPLETED_COMPOUNDS_VALID(0.0*si::celcius, p*si::pascal) // standard temperature
+            COMPLETED_COMPOUNDS_VALID(20.0*si::celcius, p*si::pascal) // room temperature
+            // COMPLETED_COMPOUNDS_VALID(100.0*si::celcius, p*si::pascal) // boiling point of water at standard pressure
+            // COMPLETED_COMPOUNDS_VALID(1000.0*si::kelvin, p*si::pascal) // melting point of aluminum
+        }
     }
 }
