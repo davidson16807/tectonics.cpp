a74b92f8d2fd68d203a972e1581b500755f35a85
Carl Davidson <diadophus@gmail.com>
2023-07-16 10:30:07 -0600

    adding checks for string cast tests

--- spike/data/string_cast_test.cpp
+++ spike/data/string_cast_test.cpp
@@ -3,16 +3,16 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch/catch.hpp"
 
-#include "Series.hpp" 
 #include "string_cast.hpp" 
 
-TEST_CASE( "many<T> string cast purity", "[many]" ) {
+TEST_CASE( "Series<T> string cast purity", "[data]" ) {
     auto a = data::series({1,2,3,4,5});
     SECTION("to_string(a) must be called repeatedly without changing the output"){
         CHECK(data::to_string(a) == data::to_string(a));
     }
 }
-TEST_CASE( "many<T> string cast correctness", "[many]" ) {
+
+TEST_CASE( "Series<T> string cast correctness", "[data]" ) {
     auto a = data::series({1,2,3,4,5,6});
     auto b = data::series({1,1,2,3,5,8});
     auto c = data::series<float>({4,8,3,8,2,4,5,9,8,2,3,5,2,1,3,3,3,1,6,1,
@@ -30,6 +30,25 @@ TEST_CASE( "many<T> string cast correctness", "[many]" ) {
         REQUIRE_THAT(stra, Catch::Contains("█"));
         REQUIRE_THAT(strb, Catch::Contains("█"));
         REQUIRE_THAT(data::to_string(data::series({INFINITY})), Catch::Contains("∞"));
-        // REQUIRE_THAT(to_string(data::series({std::sqrt(-1)})), Catch::Contains("N"));
+        REQUIRE_THAT(to_string(data::series({std::sqrt(-1)})), Catch::Contains("N"));
+    }
+}
+
+TEST_CASE( "Series<T> string cast representation", "[data]" ) {
+    auto a = data::series({1,2,3,4,5,6});
+    SECTION("to_string() must capture appreciable differences in the content of Series<T>"){
+        for(std::size_t i=0; i<a.size(); ++i){
+            auto b = data::series({1,2,3,4,5,6});
+            b[i] = -1;
+            std::string stra = data::to_string(a);
+            std::string strb = data::to_string(b);
+            CHECK(stra != strb);
+        }
+    }
+    SECTION("to_string() must capture appreciable differences in the content of Series<T>"){
+        auto c = data::series({1,2,3,4,5,6,7});
+        std::string stra = data::to_string(a);
+        std::string strc = data::to_string(c);
+        CHECK(stra != strc);
     }
 }
--- spike/data/test.cpp
+++ spike/data/test.cpp
@@ -1,3 +1,4 @@
 #include "./common_test.hpp"
 #include "./arithmetic_test.cpp"
+#include "./string_cast_test.cpp"
 #include "./exponential_test.cpp"
