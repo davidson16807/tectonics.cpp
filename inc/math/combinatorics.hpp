b415d95289b356537898b67a2e27d95620cb041c
Carl Davidson <diadophus@gmail.com>
2023-07-20 20:20:08 -0600

    more functions for combinatorics

--- inc/math/combinatorics.hpp
+++ inc/math/combinatorics.hpp
@@ -4,34 +4,55 @@
 
 namespace math {
 
-    constexpr int factorial(const int n)
+    constexpr int factorial(const int candidates)
     {
         int y = 1;
-        for (int i = 1; i <= n; ++i)
+        for (int i = 1; i <= candidates; ++i)
         {
             y *= i;
         }
         return y;
     }
 
-    constexpr int combination(const int n, const int k)
+    // number of possible sets of any size composed from a set of candidates
+    // (repetition is not allowed, order does not matter)
+    constexpr int subset(const int candidates)
     {
-        return factorial(n) / (factorial(k) * factorial(n - k));
+        return std::pow(2, candidates);
     }
 
-    constexpr int combination_with_repetition(const int n, const int k)
+    // number of possible sets of given size composed from a set of candidates
+    // (repetition is not allowed, order does not matter)
+    constexpr int combination(const int candidates, const int size)
     {
-        return combination(n+k-1, k);
+        return size > candidates? 0 : factorial(candidates) / (factorial(size) * factorial(candidates - size));
     }
 
-    constexpr int permutation(const int n, const int k)
+    // number of possible bags of given size composed from a set of candidates
+    // (repetition is allowed, order does not matter)
+    constexpr int bag(const int candidates, const int size)
     {
-        return factorial(n) / factorial(n-k);
+        return combination(candidates+size-1, size);
     }
 
-    constexpr int permutation_with_repetition(const int n, const int k)
+    // number of possible permutations of given size composed from a set of candidates
+    // (repetition is not allowed, order matters)
+    constexpr int permutation(const int candidates, const int size)
     {
-        return std::pow(n,k);
+        return size > candidates? 0 : factorial(candidates) / factorial(candidates-size);
+    }
+
+    // number of possible sequences of given size composed from a set of candidates
+    // (repetition is allowed, order matters)
+    constexpr int sequence(const int candidates, const int size)
+    {
+        return std::pow(candidates,size);
+    }
+
+    // number of possible allocations of indistiguishable resources among candidate bags
+    constexpr int allocation(const int resources, const int candidates)
+    {
+        return combination(resources+candidates-1, candidates-1);
     }
 
 }
