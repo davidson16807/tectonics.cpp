e3b4bc0fc4e8d46a2c9700fee9226ee760ab685d
Carl Davidson <diadophus@gmail.com>
2023-09-26 23:35:13 -0600

    decomposing functions in dymaxion prototype

--- spike/grid/dymaxion/dymaxion.py
+++ spike/grid/dymaxion/dymaxion.py
@@ -16,75 +16,104 @@ def cartesian_from_zlon(z,lon):
 		r*math.sin(lon),
 		z)
 
-class Subgrid:
+class Subgrids:
 	def __init__(self):
 		pass
 
-	def corner_position(self, longitude_id, z):
+	def z(self, longitude_id):
+		return 0.5-longitude_id%2
+
+	def origin_z(self, subgrid_polarity, is_polar):
+		return subgrid_polarity*[-0.5,1][is_polar]
+
+	def corner(self, longitude_id, z):
 		longitude = longitude_id*half_subgrid_longitude_arc_length
 		return cartesian_from_zlon(z,longitude)
 
-	def subgrid_id(self, sphere_position):
+	def is_inverted_subgrid_id(self, subgrid_id, is_polar):
+		return is_polar == subgrid_id%2
+
+	def is_polar_subgrid_id(self, subgrid_id, is_inverted):
+		return is_inverted == subgrid_id%2
+
+	def is_inverted_subgrid_position(self, subgrid_position):
+		return subgrid_position.x+subgrid_position.y < 1
+
+	def is_polar_sphere_position(self, sphere_position, subgrid_polarity, west, east):
+		# V3⋅(W×E)>0 indicates whether V3 occupies a polar triangle
+		return glm.dot(sphere_position, subgrid_polarity * glm.cross(west, east)) > 0
+
+	def is_eastern_sphere_position(self, sphere_position, north, south):
+		# V3⋅(N×S)>0 indicates that V3 occupies the grid indicated by Eid
+		return glm.dot(sphere_position, glm.cross(north,south)) > 0
+
+	def subgrid_polarity(self, subgrid_id):
+		return (-1)**subgrid_id
+
+	def basis(self, W,E,O, is_inverted):
+		return glm.mat3(E-O,W-O,O) if is_inverted else glm.mat3(W-O,E-O,O)
+
+	def sphere_project(self, position):
+		return glm.normalize(position)
+
+	def plane_project(self, position, normal, origin):
+		return position * glm.dot(normal, origin) / glm.dot(normal, position)
+
+class Grids:
+	def __init__(self, sugbrids):
+		self.sugbrids = sugbrids
+
+	def subgrid_position(self, sphere_position):
 		V3 = sphere_position
 		longitude = math.atan2(V3.y,V3.x)
-		longitude_id = longitude/half_subgrid_longitude_arc_length
-		Nid = 2*round(longitude_id/2)       # Nid: northernmost edge vertex id
-		Sid = 2*round((longitude_id-1)/2)+1 # Sid: southernmost edge vertex id
+		EWid = longitude/half_subgrid_longitude_arc_length
+		Nid = 2*round(EWid/2)       # Nid: northernmost edge vertex id
+		Sid = 2*round((EWid-1)/2)+1 # Sid: southernmost edge vertex id
 		Wid = min(Nid,Sid)                  # Wid: westernmost  edge vertex id
-		N = self.corner_position(Nid,  0.5)
-		S = self.corner_position(Sid, -0.5)
-		# V3⋅(N×S)>0 indicates that V3 occupies the grid indicated by Eid
-		return (Wid-1 + (glm.dot(V3, glm.cross(N,S)) > 0)) % subgrid_count
-
-	def subgrid_position(self, subgrid_id, sphere_position):
-		i = subgrid_id
-		subgrid_polarity = (-1)**i
-		V3 = sphere_position
+		N = self.sugbrids.corner(Nid,  0.5)
+		S = self.sugbrids.corner(Sid, -0.5)
+		# "i": subgrid id
+		i = (Wid-1 + self.sugbrids.is_eastern_sphere_position(V3,N,S)) % subgrid_count
+		subgrid_polarity = self.sugbrids.subgrid_polarity(i)
 		Wid = i     # west   longitude id
 		Oid = i + 1 # origin longitude id
 		Eid = i + 2 # east   longitude id
-		W = self.corner_position(Wid, 0.5-Wid%2) # W: westernmost triangle vertex
-		E = self.corner_position(Eid, 0.5-Eid%2) # E: easternmost triangle vertex
-		# V3⋅(W×E)>0 indicates whether V3 occupies a polar triangle
-		is_polar = glm.dot(V3, subgrid_polarity * glm.cross(W,E)) > 0
-		is_inverted = is_polar == i%2
-		# O: center triangle vertex, serves as origin of coordinate basis
-		O = self.corner_position(Oid, subgrid_polarity*[-0.5,1][is_polar]) 
-		# N: surface normal of the triangle
-		N = glm.cross(E-O,W-O) if is_inverted else glm.cross(W-O,E-O)
-		side_distance = glm.dot(N,O) / glm.dot(N,V3)
-		polyhedron_projection = sphere_position * side_distance
-		basis = glm.mat3(E-O,W-O,O) if is_inverted else glm.mat3(W-O,E-O,O)
-		triangle_position = glm.inverse(basis) * polyhedron_projection
-		return triangle_position.xy if is_inverted else 1-triangle_position.xy
+		W = self.sugbrids.corner(Wid, self.sugbrids.z(Wid)) # W: westernmost triangle vertex
+		E = self.sugbrids.corner(Eid, self.sugbrids.z(Eid)) # E: easternmost triangle vertex
+		is_polar = self.sugbrids.is_polar_sphere_position(V3, subgrid_polarity, W,E)
+		is_inverted = self.sugbrids.is_inverted_subgrid_id(i, is_polar)
+		O = self.sugbrids.corner(Oid, self.sugbrids.origin_z(subgrid_polarity, is_polar))
+		basis = self.sugbrids.basis(W,E,O, is_inverted)
+		N = glm.cross(basis[0], basis[1])
+		triangle_position = glm.inverse(basis) * self.sugbrids.plane_project(V3,N,O)
+		V2 = triangle_position.xy if is_inverted else 1-triangle_position.xy
+		return i, V2
 
 	def unit_sphere_position(self, subgrid_id, subgrid_position):
 		i = subgrid_id
-		is_inverted = subgrid_position.x+subgrid_position.y < 1
-		is_polar = is_inverted == i%2
+		is_inverted = self.sugbrids.is_inverted_subgrid_position(subgrid_position)
+		is_polar = self.sugbrids.is_polar_subgrid_id(i, is_inverted)
 		triangle_position = subgrid_position if is_inverted else 1-subgrid_position
-		subgrid_polarity = (-1)**i
 		V2 = triangle_position
 		Wid = i     # west   longitude id
 		Oid = i + 1 # origin longitude id
 		Eid = i + 2 # east   longitude id
-		W = self.corner_position(Wid, 0.5-Wid%2) # W: westernmost triangle vertex
-		E = self.corner_position(Eid, 0.5-Eid%2) # E: easternmost triangle vertex
-		O = self.corner_position(Oid, subgrid_polarity*[-0.5,1][is_polar]) 
-		basis = glm.mat3(E-O,W-O,O) if is_inverted else glm.mat3(W-O,E-O,O)
-		return glm.normalize(basis * glm.vec3(V2.x,V2.y,1))
+		W = self.sugbrids.corner(Wid, self.sugbrids.z(Wid)) # W: westernmost triangle vertex
+		E = self.sugbrids.corner(Eid, self.sugbrids.z(Eid)) # E: easternmost triangle vertex
+		O = self.sugbrids.corner(Oid, self.sugbrids.origin_z(self.sugbrids.subgrid_polarity(i), is_polar))
+		basis = self.sugbrids.basis(W,E,O, is_inverted)
+		return self.sugbrids.sphere_project(basis * glm.vec3(V2.x,V2.y,1))
 
 	def standardize(self, subgrid_id, subgrid_position):
 		i = subgrid_id
-		subgrid_polarity = (-1)**i
-		is_inverted = subgrid_position.x+subgrid_position.y < 1
-		is_polar = is_inverted == i%2
 		V2 = subgrid_position
+		is_inverted = self.sugbrids.is_inverted_subgrid_position(subgrid_position)
+		is_polar = self.sugbrids.is_polar_subgrid_id(i, is_inverted)
 		clampedV2 = glm.clamp(V2, 0, 1)
 		are_local = glm.equal(V2, clampedV2)
 		are_nonlocal = glm.notEqual(V2, clampedV2)
 		deviation = V2 - clampedV2
-		are_flipped = glm.equal(glm.sign(deviation), glm.vec2(subgrid_polarity))
+		are_flipped = glm.equal(glm.sign(deviation), glm.vec2(self.sugbrids.subgrid_polarity(i)))
 		is_nonlocal = are_nonlocal.x or are_nonlocal.y
 		is_flipped = are_flipped.x or are_flipped.y
 		inverted_nonlocal = (
@@ -97,7 +126,7 @@ class Subgrid:
 		return ((subgrid_id+subgrid_id_offset)%subgrid_count, flipped)
 
 
-subgrid = Subgrid()
+subgrids = Grids(Subgrids())
 samplecount = 10000
 golden_ratio = 1.618033
 golden_angle = turn/golden_ratio
@@ -109,32 +138,21 @@ unit_sphere_position = [
 	cartesian_from_zlon(z,lon)
 	for z,lon in zip(z,lon)]
 
-subgrid_id = [
-	subgrid.subgrid_id(V3)
+subgrid_position = [
+	subgrids.subgrid_position(V3)
 	for V3 in unit_sphere_position]
 
-subgrid_position = [
-	subgrid.subgrid_position(i, V3)
-	for V3,i in zip(unit_sphere_position,subgrid_id)]
-
 reconstructed = [
-	subgrid.unit_sphere_position(i, V2)
-	for V2,i in zip(subgrid_position,subgrid_id)]
+	subgrids.unit_sphere_position(i, V2)
+	for i,V2 in subgrid_position]
 
 standardize = [
-	subgrid.standardize(i, V2 + glm.vec2(0.1,0))
-	for i,V2 in zip(subgrid_id, subgrid_position)]
-
-# debug_scalar = [
-# 	subgrid.debug_scalar(i, V2 + glm.vec2(0.1,0))
-# 	for i,V2 in zip(subgrid_id, subgrid_position)]
-
-# debug_vector = [
-# 	subgrid.debug_vector(i, V2 + glm.vec2(0.1,0))
-# 	for i,V2 in zip(subgrid_id, subgrid_position)]
+	# subgrids.standardize(i, V2 + glm.vec2(-0.1,0))
+	subgrids.standardize(i, V2 + glm.vec2(0,-0.1))
+	for i,V2 in subgrid_position]
 
 standardize_sphere_position = [
-	subgrid.unit_sphere_position(*iV2)
+	subgrids.unit_sphere_position(*iV2)
 	for iV2 in standardize]
 
 def vector_aoa(vector_aos):
@@ -144,8 +162,9 @@ def vector_aoa(vector_aos):
 		[V.z for V in vector_aos]
 	]
 
-subgrid_x = [V2.x for V2 in subgrid_position]
-subgrid_y = [V2.y for V2 in subgrid_position]
+subgrid_id = [i    for i,V2 in subgrid_position]
+subgrid_x = [V2.x for i,V2 in subgrid_position]
+subgrid_y = [V2.y for i,V2 in subgrid_position]
 
 standardize_i = [i    for i,V2 in standardize]
 standardize_x = [V2.x for i,V2 in standardize]
@@ -160,9 +179,9 @@ standardize_offset = [V-N for V,N in zip(standardize_sphere_position, unit_spher
 
 domain = dict(zip('xyz', vector_aoa(unit_sphere_position)))
 
-# px.scatter_3d(**domain,color=subgrid_id).show()
-# px.scatter_3d(**domain,color=subgrid_x).show()
-# px.scatter_3d(**domain,color=subgrid_y).show()
+px.scatter_3d(**domain,color=subgrid_id).show()
+px.scatter_3d(**domain,color=subgrid_x).show()
+px.scatter_3d(**domain,color=subgrid_y).show()
 
 # px.scatter_3d(**domain,color=Nx).show()
 # px.scatter_3d(**domain,color=roundtrip_x).show()
@@ -171,10 +190,10 @@ domain = dict(zip('xyz', vector_aoa(unit_sphere_position)))
 # px.scatter_3d(**domain,color=Nz).show()
 # px.scatter_3d(**domain,color=roundtrip_z).show()
 
-# go.Figure(data=go.Cone(**domain, **dict(zip('uvw', vector_aoa(reconstructed))))).show()
+go.Figure(data=go.Cone(**domain, **dict(zip('uvw', vector_aoa(reconstructed))))).show()
 # go.Figure(data=go.Cone(**domain, **dict(zip('uvw', vector_aoa(offset))), sizeref=0.03)).show()
-# px.scatter_3d(**domain,color=distance).show()
-# px.scatter_3d(**domain,color=similarity).show()
+px.scatter_3d(**domain,color=distance).show()
+px.scatter_3d(**domain,color=similarity).show()
 go.Figure(data=go.Cone(**domain, **dict(zip('uvw', vector_aoa(standardize_offset))), sizeref=2)).show()
 px.scatter_3d(**domain,color=standardize_i).show()
 px.scatter_3d(**domain,color=standardize_x).show()
