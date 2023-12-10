0f4f63c52ea7fc165eca18f15edcc0875c6e3d7f
Carl Davidson <diadophus@gmail.com>
2023-12-10 13:00:25 -0700

    improvements to scons, scons should now build any directory

--- sconstruct.py
+++ sconstruct.py
@@ -2,20 +2,33 @@ import glob
 import os 
 cwd = GetLaunchDir()
 root = os.getcwd()
+testexe = os.path.join(cwd, 'test')
+testcpp = os.path.join(cwd, 'test.cpp')
+
+with open(testcpp, 'w') as file:
+    includes = [
+        *glob.glob(os.path.join(cwd, '**/*_specialization.hpp'), recursive=True),
+        *glob.glob(os.path.join(cwd, '**/*_test.cpp'), recursive=True),
+    ]
+    for include in includes:
+        localized = include.replace(cwd, '.')
+        file.write(f'#include "{localized}"\n')
 
 print(
 f'''
 Building executable, "test", under "{cwd}"
-using *_test.cpp files under "{cwd}"
-and sconstruct.py under "{root}".
-'''
+from newly created test.cpp file under "{cwd}"
+that includes only *_test.cpp files under "{cwd}"
+using sconstruct.py under "{root}".
+''')
+
+environment = Environment(
+    CCFLAGS="-std=c++17 -Wall -Werror -pedantic-errors -rdynamic -g -D GLM_FORCE_SWIZZLE",
+    CPPPATH=[os.path.join(root,path) for path in 'inc lib src spike'.split()],
+    LIBS='glfw GL GLEW'.split(),
 )
 
-env = Environment(
-	CCFLAGS="-std=c++17 -Wall -Werror -pedantic-errors -rdynamic -g -D GLM_FORCE_SWIZZLE",
-	CPPPATH=[os.path.join(root,path) for path in 'inc lib src spike'.split()],
-	LIBS='glfw GL GLEW'.split(),
-)
+executable = environment.Program(target=testexe, source=testcpp)
 
-env.Program(target=os.path.join(cwd, 'test'), source=glob.glob(os.path.join(cwd, "*_test.hpp")))
-env.AddPostAction(executable, './test')
+# works on some linux distros, but doesn't work on Ubuntu
+# environment.AddPostAction(executable, testexe.replace(cwd,'.'))
