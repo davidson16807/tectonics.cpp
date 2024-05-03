83cd79f9aeb62b44f5231a0bb906dd78f679f605
Carl Davidson <davidson16807@gmail.com>
2024-05-03 13:52:09 -0600

    fixing test for dymaxion::Grid

--- inc/grid/dymaxion/Grid_test.cpp
+++ inc/grid/dymaxion/Grid_test.cpp
@@ -1,4 +1,8 @@
 
+// 3rd party libraries
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch/catch.hpp"
+
 // in-house libraries
 #include <test/macros.hpp>
 #include <test/properties.hpp>
