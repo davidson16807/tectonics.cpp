723be3f2685a3a4ac943f2195c33c620ce5134ee
Carl Davidson <davidson16807@gmail.com>
2024-05-05 19:17:36 -0600

    creating separate file for LithosphereReferenceFrames

--- spike/model/rock/lithosphere/LithosphereSummarization.hpp
+++ spike/model/rock/lithosphere/LithosphereSummarization.hpp
@@ -14,69 +14,6 @@
 
 namespace rock{
 
-    /*
-    `LithosphereReferenceFrames` handles a common use case where 
-    the rasters of multiple plates must be resampled onto a single global grid system
-    so that they can interact with one another.
-    */
-    class LithosphereReferenceFrames
-    {
-
-        const dymaxion::Grid grid;
-
-    public:
-
-        LithosphereReferenceFrames(const dymaxion::Grid& grid): grid(grid)
-        {}
-
-        template<typename Raster>
-        void globalize(
-            const std::vector<Plate>& plates,
-            const std::vector<Raster>& local,
-            std::vector<Raster>& global
-        ) const {
-            for (std::size_t i = 0; i < plates.size(); ++i)
-            {
-                /*
-                            grid      rotation    grid
-                resample = id ⟶ position ⟶ position ⟶ id
-                */
-                auto resample = 
-                    series::map(
-                        field::compose(
-                            dymaxion::NearestVertexId(grid), 
-                            field::Affinity(plates[i].local_to_global)),
-                        dymaxion::VertexPositions(grid)
-                    );
-                each::get(raster, resample, global[i]);
-            }
-        }
-
-        template<typename Raster>
-        void localize(
-            const std::vector<Plate> plates,
-            const Raster& global,
-            std::vector<Raster>& local
-        ) const {
-            for (std::size_t i = 0; i < plates.size(); ++i)
-            {
-                /*
-                            grid      rotation    grid
-                resample = id ⟶ position ⟶ position ⟶ id
-                */
-                auto resample = 
-                    series::map(
-                        field::compose(
-                            dymaxion::NearestVertexId(grid), 
-                            field::Affinity(plates[i].global_to_local)),
-                        dymaxion::VertexPositions(grid)
-                    );
-                each::get(global, resample, local[i]);
-            }
-        }
-
-    };
-
     class LithosphereSummarization
     {
 
@@ -121,9 +58,9 @@ namespace rock{
     /*
     basic use case:
     summarization.summarize (plates, locals, scratch)      // summarize each plate into a (e.g.) FormationSummary raster
-    coordinates.globalize   (plates, locals, globals)      // resample plate-specific rasters onto a global grid
+    frames     .globalize   (plates, locals, globals)      // resample plate-specific rasters onto a global grid
     summarization.condense  (globals, master)              // condense globalized rasters into e.g. LithosphereSummary
-    coordinates.localize    (plates,  master, derivatives) // resample global raster to a plate-specific for each plate
+    frames     .localize    (plates,  master, derivatives) // resample global raster to a plate-specific for each plate
     */
 
 }
