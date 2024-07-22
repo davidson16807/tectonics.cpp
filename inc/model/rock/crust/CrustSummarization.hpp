0a1e29793b74aa341b8d66bae5e4b2d2362c2a1e
Carl Davidson <davidson16807@gmail.com>
2024-07-22 12:32:10 -0600

    reverting change caused by misunderstanding of CrustSummarization

--- inc/model/rock/crust/CrustSummarization.hpp
+++ inc/model/rock/crust/CrustSummarization.hpp
@@ -7,23 +7,23 @@ namespace rock{
     class CrustSummarization
     {
         const FormationSummarization summarize;
-        const CrustSummaryOps ops;
+        const FormationSummaryOps ops;
     public:
-        CrustSummarization(const FormationSummarization& summarize, const CrustSummaryOps& ops):
+        CrustSummarization(const FormationSummarization& summarize, const FormationSummaryOps& ops):
             summarize(summarize),
             ops(ops)
         {}
         void operator() (
             const int plate_id, 
             const Crust& crust, 
-            CrustSummary& out, 
+            FormationSummary& out, 
             FormationSummary& scratch
         ) const {
             ops.reset(out);
             for (int i = 0; i < crust.size(); ++i)
             {
                 summarize(plate_id, crust[i], scratch);
-                ops.absorb(out, scratch, out);
+                ops.combine(out, scratch, out);
             }
         }
     };
