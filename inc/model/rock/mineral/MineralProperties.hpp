28bfaad2e3b85084622c0990e17730a071c46661
Carl Davidson <davidson16807@gmail.com>
2024-07-27 16:55:06 -0600

    reinstating MineralProperties since I hadn't realized it was used by inc

---
+++ inc/model/rock/mineral/MineralProperties.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+namespace rock{
+
+    struct MineralMass
+    {
+        constexpr MineralMass(){}
+        template<typename Minerallike>
+        float operator() (const std::size_t i, const Minerallike& constituent) const 
+        {
+            return constituent.mass;
+        }
+    };
+
+    template<typename MolarMassTable>
+    struct MineralMoles
+    {
+        MolarMassTable molar_masses;
+        MineralMoles(const MolarMassTable& molar_masses):
+            molar_masses(molar_masses)
+        {}
+        template<typename Minerallike>
+        float operator() (const std::size_t i, const Minerallike& constituent) const 
+        {
+            return constituent.mass / molar_masses[i];
+        }
+    };
+
+    template<typename DensityTable>
+    struct MineralVolume
+    {
+        DensityTable densities;
+        MineralVolume(const DensityTable& densities):
+            densities(densities)
+        {}
+        template<typename Minerallike>
+        float operator() (const std::size_t i, const Minerallike& constituent) const 
+        {
+            return constituent.mass / densities[i];
+        }
+    };
+
+}
+
