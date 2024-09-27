e6cf8a3712e87dd9e2efd50f46718b28c02b94f8
Carl Davidson <davidson16807@gmail.com>
2024-09-27 12:05:07 -0600

    moving voronoi demo to its own folder and stripping code to essentials

---
+++ demo/voronoi/_test.cpp
@@ -0,0 +1,196 @@
+
+// std libraries
+#include <iostream>
+#include <string>
+
+// gl libraries
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+// glm libraries
+#define GLM_FORCE_PURE      // disable anonymous structs so we can build with ISO C++
+
+#include <math/glm/special_specialization.hpp>
+#include <math/glm/special.hpp>
+#include <math/analytic/Sum.hpp>
+#include <math/analytic/Gaussian.hpp>
+#include <math/analytic/Error.hpp>
+#include <math/inspected/InverseByNewtonsMethod.hpp>
+
+// in house libraries
+#include <index/series/Map.hpp>
+#include <index/series/Uniform.hpp>
+#include <index/glm/each.hpp>                       // get
+#include <index/each.hpp>                           // get
+#include <index/glm/known.hpp>                      // greaterThan
+#include <index/known.hpp>                          // greaterThan
+#include <index/whole.hpp>                          // max, mean
+#include <index/series/Range.hpp>                   // Range
+#include <index/series/noise/UnitIntervalNoise.hpp> // UnitIntervalNoise
+#include <index/series/noise/glm/UnitVectorNoise.hpp>
+#include <index/series/noise/GaussianNoise.hpp>
+#include <index/adapted/symbolic/SymbolicArithmetic.hpp>
+#include <index/adapted/symbolic/SymbolicOrder.hpp>
+#include <index/adapted/si/SiStrings.hpp>
+#include <index/adapted/glm/GlmMetric.hpp>
+#include <index/aggregated/Order.hpp>
+#include <index/iterated/Nary.hpp>
+
+#include <field/Compose.hpp>                        // Compose
+#include <field/noise/RankedFractalBrownianNoise.hpp> // dymaxion::RankedFractalBrownianNoise
+
+#include <relation/ScalarRelation.hpp>
+
+#include <buffer/PyramidBuffers.hpp>                // buffer::PyramidBuffers
+
+#include <grid/dymaxion/Grid.hpp>                   // dymaxion::Grid
+#include <grid/dymaxion/series.hpp>                 // dymaxion::BufferVertexIds
+#include <grid/dymaxion/buffer/WholeGridBuffers.hpp>// dymaxion::WholeGridBuffers
+
+#include <raster/unlayered/VectorCalculusByFundamentalTheorem.hpp> // unlayered::VectorCalculusByFundamentalTheorem
+#include <raster/unlayered/FloodFilling.hpp>        // unlayered::FloodFilling
+#include <raster/unlayered/Voronoi.hpp>             // unlayered::Voronoi
+#include <raster/unlayered/ImageSegmentation.hpp>   // unlayered::ImageSegmentation
+
+// #include <model/rock/stratum/StratumGenerator.hpp>  // StratumGenerator
+
+#include <update/OrbitalControlState.hpp>           // update::OrbitalControlState
+#include <update/OrbitalControlUpdater.hpp>         // update::OrbitalControlUpdater
+
+#include <view/ColorscaleSurfaceShaderProgram.hpp>  // view::ColorscaleSurfaceShaderProgram
+#include <view/IndicatorSwarmShaderProgram.hpp>     // view::IndicatorSwarmShaderProgram
+#include <view/MultichannelSurfaceShaderProgram.hpp>// view::MultichannelSurfaceShaderProgram
+
+int main() {
+  // initialize GLFW
+  if (!glfwInit()) {
+    std::cout << stderr << " ERROR: could not start GLFW3" << std::endl;
+    return 1;
+  }
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // needed for MacOS
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // we don't want the old OpenGL
+
+  // open a window
+  GLFWwindow* window = glfwCreateWindow(850, 640, "Hello Plates", NULL, NULL);
+  if (!window) {
+    std::cout << stderr << " ERROR: could not open window with GLFW3" << std::endl;
+    glfwTerminate();
+    return 1;
+  }
+
+  // create a GLEW context
+  glfwMakeContextCurrent(window);
+
+  // initialize GLEW 
+  glewExperimental = GL_TRUE;
+  if (glewInit() != GLEW_OK) {
+    std::cout << stderr << " ERROR: Failed to initialize GL" << std::endl;
+    return -1;
+  }
+
+  // get version info
+  const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
+  const GLubyte* version = glGetString(GL_VERSION); // version as a string
+  printf("Renderer: %s\n", renderer);
+  printf("OpenGL version supported %s\n", version);
+
+  // tell GL to only draw onto a pixel if the shape is closer to the viewer
+  glEnable(GL_DEPTH_TEST); // enable depth-testing
+  glDepthFunc(GL_LESS); // depth-testing interprets a smaller value as "closer"
+
+  /* OUR STUFF GOES HERE NEXT */
+  float radius(3.0f);
+  int vertices_per_square_side(32);
+  dymaxion::Grid grid(radius, vertices_per_square_side);
+  dymaxion::VertexPositions vertex_positions(grid);
+
+  // flatten raster for OpenGL
+  dymaxion::WholeGridBuffers<int,float> grids(vertices_per_square_side);
+  std::vector<float> buffer_color_values(grid.vertex_count());
+  std::vector<float> buffer_uniform(grid.vertex_count(), 1.0f);
+  std::vector<glm::vec3> buffer_positions(grid.vertex_count());
+  std::vector<unsigned int> buffer_element_vertex_ids(grids.triangle_strips_size(vertex_positions));
+
+  auto metric = iterated::Metric{adapted::GlmMetric{}};
+  auto copy = iterated::Identity();
+  auto voronoi = unlayered::Voronoi{adapted::GlmMetric{}};
+  std::vector<std::uint8_t> nearest_plate_id(grid.vertex_count());
+  using vec3 = glm::vec3;
+  std::vector<vec3>plate_seeds{
+    vec3( 1, 1, 1),
+    vec3( 1, 1,-1),
+    vec3( 1,-1, 1),
+    vec3( 1,-1,-1),
+    vec3(-1, 1, 1),
+    vec3(-1, 1,-1),
+    vec3(-1,-1, 1),
+    vec3(-1,-1,-1)
+  };
+  metric.normalize(plate_seeds, plate_seeds);
+  voronoi(vertex_positions, plate_seeds, nearest_plate_id);
+
+  copy(nearest_plate_id, buffer_color_values);
+  copy(vertex_positions, buffer_positions);
+  grids.storeTriangleStrips(series::range<unsigned int>(grid.vertex_count()), buffer_element_vertex_ids);
+
+  // initialize control state
+  update::OrbitalControlState control_state;
+  control_state.min_zoom_distance = 1.0f;
+  control_state.log2_height = 2.5f;
+  control_state.angular_position = glm::vec2(45.0f, 30.0f) * 3.14159f/180.0f;
+  
+  // initialize view state
+  view::ViewState view_state;
+  view_state.projection_matrix = glm::perspective(
+    3.14159f*45.0f/180.0f, 
+    850.0f/640.0f, 
+    1e-3f, 1e16f
+  );
+  view_state.view_matrix = control_state.get_view_matrix();
+  view::ColorscaleSurfacesViewState colorscale_state;
+  colorscale_state.max_color_value = whole::max(buffer_color_values);
+
+  // initialize shader program
+  view::ColorscaleSurfaceShaderProgram colorscale_program;  
+
+  // initialize MessageQueue for MVU architecture
+  messages::MessageQueue message_queue;
+  message_queue.activate(window);
+
+  while(!glfwWindowShouldClose(window)) {
+      // wipe drawing surface clear
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+      colorscale_program.draw(
+        buffer_positions,    // position
+        buffer_color_values, // color value
+        buffer_uniform,      // displacement
+        buffer_uniform,      // darken
+        buffer_uniform,      // culling
+        buffer_element_vertex_ids,
+        colorscale_state,
+        view_state,
+        GL_TRIANGLE_STRIP
+      );
+
+      // put stuff we've been drawing onto the display
+      glfwSwapBuffers(window);
+
+      std::queue<messages::Message> message_poll = message_queue.poll();
+      while (!message_poll.empty())
+      {
+        update::OrbitalControlUpdater::update(control_state, message_poll.front(), control_state);
+        message_poll.pop();
+      }
+      // control_state.angular_position.x += 1.0f * 3.1415926f/180.0f;
+      view_state.view_matrix = control_state.get_view_matrix();
+  }
+
+  // close GL context and any other GLFW resources
+  message_queue.deactivate(window);
+  glfwTerminate();
+  return 0;
+}
