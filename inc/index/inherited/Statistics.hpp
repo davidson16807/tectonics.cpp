1b92e0da7415c90b097199829cbd1c8be95524b9
Carl Davidson <davidson16807@gmail.com>
2025-04-15 21:32:46 -0600

    introducing new `inherited` namespace for operations on depth traversals

---
+++ inc/index/inherited/Statistics.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+// C libraries
+#include <assert.h>  /* assert */
+
+// in-house libraries
+
+namespace inherited
+{
+
+	template <typename ElementArithmetic>
+	class Statistics
+	{
+		const ElementArithmetic arithmetic;
+		const iterated::Identity copy;
+	public:
+		Statistics(const ElementArithmetic& arithmetic):
+			arithmetic(arithmetic),
+			copy(copy)
+		{}
+
+		template <typename Ids, typename Out>
+		void count (const Ids& parent_ids, Out& out)
+		{
+			copy(a, out);
+			auto size = a.size();
+			for (std::size_t i = size-1; i >= 0; --i)
+			{
+				out[i] = out[parent_ids[i]] + 1;
+			}
+		}
+
+		template <typename Ids, typename In, typename Out>
+		void sum (const Ids& parent_ids, const In& a, Out& out)
+		{
+			copy(a, out);
+			auto size = a.size();
+			for (std::size_t i = size-1; i >= 0; --i)
+			{
+				out[i] = arithmetic.add(out[parent_ids[i]], a[i]);
+			}
+		}
+
+		template <typename Ids, typename In, typename Out>
+		void product (const Ids& parent_ids, const In& a, Out& out)
+		{
+			copy(a, out);
+			auto size = a.size();
+			for (std::size_t i = size-1; i >= 0; --i)
+			{
+				out[i] = arithmetic.multiply(out[parent_ids[i]], a[i]);
+			}
+		}
+
+	};
+
+}
