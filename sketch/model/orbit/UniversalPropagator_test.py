a2f8d4d48fe0ae15aaecc64d79618ef94bf4960e
Carl Davidson <davidson16807@gmail.com>
2025-11-18 12:33:01 -0700

    UniversalPropagator tests now consider other starting positions
    also adding more real life sample orbits

--- sketch/model/orbit/UniversalPropagator_test.py
+++ sketch/model/orbit/UniversalPropagator_test.py
@@ -11,8 +11,14 @@ from Universals import Universals
 from UniversalPropagator import UniversalPropagator
 
 G = 6.6743015e-11 # m³kg⁻¹s⁻²
-m_sun = 1.32712440018e20/G # kg
-m_galaxy = 1.262e41 * G # kg, back calculated to achieve period of 250 million years
+mass_of_earth_moon = 6.0457e24 # kg
+mass_of_jupiter = 1.898e27 # kg
+mass_of_saturn = 5.683e26 # kg
+mass_of_uranus = 8.681e25 # kg
+mass_of_neptune = 1.024e26 # kg
+mass_of_pluto_charon = 1.3e22 + 1.5e21 # kg
+mass_of_sun = 1.32712440018e20/G # kg
+mass_of_galaxy = 1.262e41 * G # kg, back calculated to achieve period of 250 million years
 au = 1.5e11
 
 propagator = UniversalPropagator(G, max_refinement_count=100, laguerre_method_n = 5)
@@ -46,24 +52,24 @@ converter = ElementsAndState(properties)
 # # 	glm.vec3(0.0,9.2e3,0.0)) # from example 2.2 in Conway & Prussing
 # # plot(satellite, 60*60*24*30, sample_count = 3000) # currently doesn't work
 
-# halley = Universals(m_sun, 0.0, glm.vec3(0.59*au,0.0,0.0), glm.vec3(0.0,70.56e3,0.0))
+# halley = Universals(mass_of_sun, 0.0, glm.vec3(0.59*au,0.0,0.0), glm.vec3(0.0,70.56e3,0.0))
 # plot(halley, 60*60*24*365*80, sample_count = 6000)
 
 # moon = orbit = Universals(3.986004418e14/G, 0.0, glm.vec3(384399e3,0.0,0.0), glm.vec3(0.0,1.022e3,0.0))
 # plot(moon, 60*60*24*30)
 
-# earth = Universals(m_sun, 0.0, glm.vec3(au,0.0,0.0), glm.vec3(0.0,29.7e3,0.0))
+# earth = Universals(mass_of_sun, 0.0, glm.vec3(au,0.0,0.0), glm.vec3(0.0,29.7e3,0.0))
 # plot(earth, 60*60*24*365)
 
-# eccentric_solar = Universals(m_sun, 0.0,	glm.vec3(0.59*au,0.0,0.0), glm.vec3(0.0,54e3,0.0))
+# eccentric_solar = Universals(mass_of_sun, 0.0,	glm.vec3(0.59*au,0.0,0.0), glm.vec3(0.0,54e3,0.0))
 # plot(eccentric_solar, 60*60*24*365*80)
 
-# hyperbolic_solar = Universals(m_sun, 0.0,glm.vec3(0.59*au,0.0,0.0), glm.vec3(0.0,55e3,0.0))
+# hyperbolic_solar = Universals(mass_of_sun, 0.0,glm.vec3(0.59*au,0.0,0.0), glm.vec3(0.0,55e3,0.0))
 # plot(hyperbolic_solar, 60*60*24*365*80)
 
 # breakpoint()
 
-mars = Universals(m_sun, 0.0,
+mars = Universals(mass_of_sun, 0.0,
 	glm.vec3(1.3814*au,0.0,0.0), 
 	glm.vec3(0.0,24.07e3,0.0))
 # plot(mars, 60*60*24*365*3)
@@ -87,44 +93,44 @@ test cases:
 		hyperbolic solar satellite
 '''
 
+pi = 3.141592653589793238462643383279
+degrees = 2*pi/360
+
 elliptics = [
-    (m_sun, Elements(5.790905e10,   0.2056, 0.0, 0.0, 0.0, 0.0)), # Mercury
-    (m_sun, Elements(1.082080e11,   0.0068, 0.0, 0.0, 0.0, 0.0)), # Venus
-    (m_sun, Elements(1.49598023e11, 0.0167, 0.0, 0.0, 0.0, 0.0)), # Earth
-    (m_sun, Elements(2.279392e11,   0.0934, 0.0, 0.0, 0.0, 0.0)), # Mars
-    (m_sun, Elements(7.78570e11,    0.0489, 0.0, 0.0, 0.0, 0.0)), # Jupiter
-    (m_sun, Elements(1.43353e12,    0.0565, 0.0, 0.0, 0.0, 0.0)), # Saturn
-    (m_sun, Elements(2.87246e12,    0.046,  0.0, 0.0, 0.0, 0.0)), # Uranus
-    (m_sun, Elements(4.49506e12,    0.009,  0.0, 0.0, 0.0, 0.0)), # Neptune
-    # m_galaxy, Elements(), # sun around galaxy
+	(mass_of_earth_moon, Elements(384e6,  0.0549, 0.0*degrees, 0.0, 0.0, 0.0)), # Moon
+	(mass_of_jupiter, Elements(421.8e6,   0.0041, 0.050*degrees, 0.0, 0.0, 0.0)), # Io
+	(mass_of_jupiter, Elements(671.1e6,   0.0090, 0.470*degrees, 0.0, 0.0, 0.0)), # Europa
+	(mass_of_jupiter, Elements(1070.4e6,  0.0013, 0.200*degrees, 0.0, 0.0, 0.0)), # Ganymede
+	(mass_of_jupiter, Elements(1882.7e6,  0.0074, 0.192*degrees, 0.0, 0.0, 0.0)), # Callisto
+	# TODO: saturnian moons
+	# (mass_of_saturn, Elements(421.8e6,   0.0041, 0.050*degrees, 0.0, 0.0, 0.0)), # Io
+    (mass_of_sun, Elements(5.790905e10,   0.2056, 7.005*degrees, 0.0, 0.0, 0.0)), # Mercury
+    (mass_of_sun, Elements(1.082080e11,   0.0068, 3.3947*degrees, 0.0, 0.0, 0.0)), # Venus
+    (mass_of_sun, Elements(1.49598023e11, 0.0167, 0.000*degrees, 0.0, 0.0, 0.0)), # Earth
+    (mass_of_sun, Elements(2.279392e11,   0.0934, 1.851*degrees, 0.0, 0.0, 0.0)), # Mars
+    (mass_of_sun, Elements(7.78570e11,    0.0489, 1.305*degrees, 0.0, 0.0, 0.0)), # Jupiter
+    (mass_of_sun, Elements(1.43353e12,    0.0565, 2.484*degrees, 0.0, 0.0, 0.0)), # Saturn
+    (mass_of_sun, Elements(2.87246e12,    0.046,  0.770*degrees, 0.0, 0.0, 0.0)), # Uranus
+    (mass_of_sun, Elements(4.49506e12,    0.009,  1.769*degrees, 0.0, 0.0, 0.0)), # Neptune
+    (mass_of_sun, Elements(39.482*au,     0.2488, 17.14*degrees, 0.0, 0.0, 0.0)), # Pluto
+    # mass_of_galaxy, Elements(), # sun around galaxy
 ]
 
-'''
-"circular orbit" here is in reference to Stepanov's mathematical "orbit" property from "Elements of Programming",
-and should not be confused with physically circular orbits.
-'''
-def test_nth_period_circular_orbit_of_n():
-	for (mass, elements) in elliptics:
-		for n in range(1,9):
-			T = properties.period_from_semi_major_axis(elements.semi_major_axis, mass)
-			original = Universals.from_state(mass, converter.get_state_from_elements(elements, mass))
-			updated = original
-			for i in range(n):
-				updated = Universals.from_state(mass, propagator.state(updated,T/n))
-		assert state_distance(State.from_universals(updated), State.from_universals(original)) < 1e-5
-
-def test_nth_period_congruence_modulo_n():
-	for (mass, elements) in elliptics:
-		n = 10
-		# TODO: FIX DIV0 WHERE I=0
-		for i in range(1,n+1):
-			T = properties.period_from_semi_major_axis(elements.semi_major_axis, mass)
-			universals = Universals.from_state(mass, converter.get_state_from_elements(elements, mass))
-			for j in range(1,5+1):
-				assert state_distance(
-					propagator.state(universals,T*(i/n)),
-					propagator.state(universals,T*(i/n+j)), 
-				) < 3e-5
+def test_nth_period_congruence_modulo_n(): # also checks Stepanov's "circular orbit" property and an identity
+	n = 10 # samples of orbit
+	for k in range(n): # starting position
+		# TODO: vary starting position in the orbit
+		for (mass, elements) in elliptics:
+			elements.mean_anomaly = 2*pi*k/n
+			# TODO: FIX DIV0 WHERE I=0
+			for i in range(1,n+1):
+				T = properties.period_from_semi_major_axis(elements.semi_major_axis, mass)
+				universals = Universals.from_state(mass, converter.get_state_from_elements(elements, mass))
+				for j in range(0,3+1):
+					assert state_distance(
+						propagator.state(universals,T*(i/n)),
+						propagator.state(universals,T*(i/n+j)), 
+					) < 3e-5
 
 '''
 for any orbit:
@@ -157,16 +163,15 @@ for any orbit:
 	angular momentum is conserved
 		angular momentum(advance(o,t)) == angular momentum(o)
 
-for known elliptic orbits:
-	congruence:
-		state(o,n*T) = o for any n∈ℕ
-	involute: 
-		advance(advance(o, T/2), T/2) = o
-	orbit: 
-		advance(advance(advance(o, T/3), T/3), T/3) = o etc.
-	state(periapsis,T/2).position = apoapsis
 	t=0 minimizes glm.length(o↦state(periapsis,t).position)
 	t=0 maximizes glm.length(o↦state(periapsis,t).velocity)
+
+for known elliptic orbits:
+x	congruence:
+		state(o,n*T) = o for any n∈ℕ
+x	orbit/involute/identity: 
+		advance(advance(advance(o, T/n), T/n), T/n) = o etc.
+	state(periapsis,T/2).position = apoapsis
 	t=0 maximizes glm.length(o↦state(apoapsis,t).position)
 	t=0 minimizes glm.length(o↦state(apoapsis,t).velocity)
 
