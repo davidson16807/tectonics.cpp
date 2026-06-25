ec216aa309a0f204a04ceff5e7566abb5f709baa
Carl Davidson <davidson16807@gmail.com>
2026-06-24 21:50:57 -0600

    introducing new `EntityComponents` class to generalize `TrackPositions`
    also to allow components with many-to-one relationships with entities

---
+++ inc/model/orrery/EntityComponents.hpp
@@ -0,0 +1,83 @@
+#pragma once
+
+#include <cassert>       // assert
+
+#include <utility>      // std::pair
+#include <vector>       // std::vector
+
+/*
+`EntityComponents` is an alternate implementation of the `*Components` interface
+that stores tuples of entity ids and their components using std::pair<id,Component>.
+Some operations are O(N) so they are not included.
+These operations that are common to other `*Component` classes, 
+chiefly `has()`, `remove()`, `entity_destroyed()`, `component_for_entity()`, and `complete()`.
+A consequence of this is that a single entity may possess multiple components of the same type,
+since O(N) inclusion tests like `has()` are not performed.
+
+`EntityComponents` is best suited to represent a table of components 
+within an Entity-Component-System ("ECS") pattern where components are sparse and continously regenerated,
+or tables where one entity could own many components of the same type.
+*/
+
+namespace orrery
+{
+
+template<typename id, typename Component>
+struct EntityComponents
+{
+
+	std::vector<std::pair<id,Component>> components;
+
+	/*
+	Create an empty `EntityComponents<id,Component>` instance.
+	*/
+	EntityComponents():
+		components()
+	{
+	}
+
+	void clear()
+	{
+		components.clear();
+	}
+
+	void resize(std::size_t size)
+	{
+		components.resize(size);
+	}
+
+	void add(const id entity, const Component& component)
+	{
+		components.emplace_back(entity, component);
+	}
+
+	template<Component2>
+	void add(const EntityComponents<id,Component2>& other)
+	{
+		for (int i = 0; i < other.components.size(); ++i)
+		{
+			components.emplace_back(other.components[i].first, other.components[i].second);
+		}
+	}
+
+	// `entity_count` returns the number of entities tracked by this component store
+	std::size_t entity_count() const
+	{
+		return components.size();
+	}
+
+	// `size` mirrors std::vector-style sizing for callers that traverse entity slots directly.
+	std::size_t size() const
+	{
+		return components.size();
+	}
+
+	const id entity_for_index(std::size_t component) const
+	{
+		return components[component].first;
+	}
+
+};
+
+}
+
