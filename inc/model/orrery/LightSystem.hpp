1a3ff0edc35b27c409cc410d55fef25cb3453f0f
Carl Davidson <davidson16807@gmail.com>
2026-07-07 16:40:34 -0600

    moving LightExposure to its own file

--- inc/model/orrery/LightSystem.hpp
+++ inc/model/orrery/LightSystem.hpp
@@ -16,45 +16,10 @@
 // in-house libraries
 #include "Spin.hpp"
 #include "EntityComponents.hpp"
+#include "LightExposure.hpp"
 
 namespace orrery
 {
-
-    template<typename Luminosity, typename Temperature>
-    struct PointLightSource{
-
-        Luminosity luminosity;
-        Temperature temperature;
-
-        PointLightSource(
-            const Luminosity luminosity,
-            const Temperature temperature
-        ): 
-            luminosity(luminosity),
-            temperature(temperature)
-        {}
-    };
-
-    template<typename scalar, typename Time, typename LightSource, glm::qualifier precision = glm::defaultp>
-    struct LightExposure{
-
-        using vec3 = glm::vec<3,scalar,precision>;
-
-        LightSource source;
-        vec3 offset;
-        Time time;
-
-        LightExposure(
-            const LightSource& source,
-            const vec3& offset,
-            const Time& time
-        ):
-            source(source),
-            offset(offset),
-            time(time)
-        {}
-
-    };
 
     template<typename id, typename scalar, typename Time, typename LightSource, glm::qualifier precision = glm::defaultp>
     class LightSystem{
