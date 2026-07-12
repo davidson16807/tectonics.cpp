30efa501f31e98d90182ae858158ec0696a99e65
Carl Davidson <davidson16807@gmail.com>
2026-07-12 12:20:41 -0600

    adding luminosity() method to BlackBodyPointSource
    This is in anticipation of other light sources that model light as vector-like objects of power for wavelength bins.

--- inc/model/light/BlackBodyPointSource.hpp
+++ inc/model/light/BlackBodyPointSource.hpp
@@ -3,10 +3,12 @@
 namespace light{
 
     template<typename Power, typename Temperature>
-    struct BlackBodyPointSource{
+    class BlackBodyPointSource{
 
         Power luminosity;
         Temperature temperature;
+
+    public:
 
         BlackBodyPointSource(
             const Power luminosity,
@@ -15,6 +17,12 @@ namespace light{
             luminosity(luminosity),
             temperature(temperature)
         {}
+
+        Power luminosity() const
+        {
+            // TODO: return an array of power for wavelength bins according to Wein's law.
+            return luminosity;
+        }
     };
 
 }
